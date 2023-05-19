bdf6b6530458cbb9776e5cf3bcd6dbccd6485d47
EdwardCatoiu <ecatoiu@eng.ucsd.edu>
2023-05-19 00:17:53 -0700

    demo updated

--- _figuresModule/_figS8.py
+++ _figuresModule/_figS8.py
@@ -33,7 +33,7 @@ def figS8_B(proteinTargets,fig = False, ax = False, save = False):
     set2 = set(cobrame)
     set3 = set(ecocyc)
 
-    v = venn3_unweighted([set1, set2, set3], ("Monomer",'COBRAME',  'ECOCYC'))
+    v = venn3_unweighted([set1, set2, set3], ("Monomer",'COBRAME',  'ECOCYC'),ax=ax)
     ax.set_title("Annotated Oligomerization of Protein Complex ")
     outfile = op.join(qspaceDirs['FiguresOutput_dir'], 'FigS8B-003A-AnnotatedOligomerizationfProteinComplexes.png')
 
@@ -50,7 +50,7 @@ def figS8_C(hetero_oligo_pdb, homo_oligo_pdb,homo_oligo_swiss,fig = False, ax = 
     set2 = set(homo_oligo_pdb.keys())
     set3 = set(homo_oligo_swiss.keys())
 
-    venn3_unweighted([set1, set2, set3], ('Hetero-k-mer (PDB)','Homo-k-mer (PDB)',  'Homo-k-mer (SWISS)'))
+    venn3_unweighted([set1, set2, set3], ('Hetero-k-mer (PDB)','Homo-k-mer (PDB)',  'Homo-k-mer (SWISS)'),ax=ax)
     ax.set_title("Structural Evidence of Oligomerization\nfor {} 'MONOMER' Enzymes".format(len(set1.union(set2).union(set3))))
     # c=venn3_circles([set1, set2, set3], linestyle='-', linewidth=1, color="k")
     
@@ -81,12 +81,14 @@ def figS8_D(df_recipes_changed,fig = False, ax = False, save = False):
         xy = np.array((i, len(dfr[dfr.stoichChanged == True])))
         ax.annotate(len(dfr[dfr.stoichChanged == True]), xy = xy + np.array([0.1,10]))
         i+=1
-
-    ax.set_xticklabels('')
-    ax.set_xticks(np.array(range(10) )+ 0.4)
+    ax.set_xlabel('Oligomer Re-Annotation Case Id')
+    ax.set_ylabel('Number of Proteins')
+    ax.set_xticklabels(xlabel)
+    ax.set_xticks(np.array(range(10) ))
     ax.tick_params(axis='both', which='major', labelsize=14,length = 8, width = 1.5,)
     ax.tick_params(axis='both', which='minor', labelsize=14,length = 4, width = 1.5)
     
     outfile = op.join(qspaceDirs['FiguresOutput_dir'], 'FigS8D-003B-ResultsOfStoichReannotation.png')
     if save:
         fig.savefig(outfile,dpi = 900,transparent = True, bbox_inches = 'tight')
+    return fig, ax
